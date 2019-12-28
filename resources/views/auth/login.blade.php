6d9f8d91c217f76da7451eb2fd7615e5e3757e37
masa555 <tyutyumasato@gmail.com>
2019-12-29 03:43:23 +0900

    google-login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,7 +29,13 @@
             </span>
         @endif
       </div>
-
+      <div class="form-group row mt-2">
+      <div class="col-md-8 offset-md-4">
+           <a href="auth/google" class="btn btn-secondary" role="button">
+            Google Login
+          </a>
+        </div>
+      </div>
       <div class="form-group text-center">
         <input type="submit" name="commit" value="ログインする" class="loginBtn" data-disable-with="ログインする">
       </div>
